f2d61397b078b96454d9fe3516dcf663354f320f
Tim Overbay <tim.overbay@gmail.com>
2022-07-16 21:18:53 -0700

    Modified premake

--- premake5.lua
+++ premake5.lua
@@ -7,22 +7,22 @@ project "GLFW"
 
     files
     {
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
 
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
 
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c",
     }
     filter "system:linux"
         pic "On"
@@ -32,16 +32,16 @@ project "GLFW"
 
         files
         {
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
         }
 
         defines
@@ -55,16 +55,16 @@ project "GLFW"
 
         files
         {
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_module.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
 
         defines 
@@ -73,10 +73,10 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-		links
-		{
-			"Dwmapi.lib"
-		}
+        links
+        {
+            "Dwmapi.lib"
+        }
 
     filter "configurations:Debug"
         runtime "Debug"
